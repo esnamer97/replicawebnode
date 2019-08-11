1c1c23e9d39c838eecf133d919aaba64270b93b3
brayan <brayanhernandezmartinez@gmail.com>
2019-08-11 11:11:48 -0500

    Replica terminada

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const indexRoute = require('./routes/');
 
 //settings
-app.set('port', 4000);
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','ejs');
 
 //Middlewar
 
 
 //routes
-app.get('/', (req, res) => {
-    res.send("Hello world");
-});
+app.use(indexRoute);
 
 //Static Files
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Listening the server
 app.listen(app.get('port'),() => {
